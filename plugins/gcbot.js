e34c63ffb847858ba0ee8dc4e4fdc519e9ef8d31
Tiosat021 <102357958+Tiosat021@users.noreply.github.com>
2022-03-26 11:12:53 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,13 +2,19 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-╭═══════════════════════
-║╭──❉ 〔 ⳹ ❋ཻུ۪۪⸙𝙕𝙞𝙛𝙖𝙗𝙤𝙩𝙯⳹ ❋ཻུ۪۪ 〕 ❉────── 
-║│➸ *_GROUP OFFC ZIFABOTZ_*
-║│➸  *https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv*
+╭═══════════❉
+║│➸「 Grup Bot 1 」
+║│• https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
+║│
+║│➸「 Grup Bot 2 」
+║│• https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+║│
+║│➸「 Grup Bot 3 」
+║│• https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
+║│
+║│➸「 Grup Bot 4 」
+║│• https://chat.whatsapp.com/I2VmKa5oZ7o7wLa4bRTxQJ
 ╰─────────❉
-▌│█║▌║▌║║▌║▌║█│▌
-⳹ ❋ཻུ۪۪⸙𝙕𝙞𝙛𝙖𝙗𝙤𝙩𝙯⳹ ❋ཻུ۪۪⸙ by.rozi
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
 }
