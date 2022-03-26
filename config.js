159460791ddb2362fad5db75b2d15d68d600a6ab
Tiosat021 <102357958+Tiosat021@users.noreply.github.com>
2022-03-26 16:16:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,14 +50,14 @@ global.wait = '「 ⏱️ 」Proses...'
 global.eror = '「❗」Server Error'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© SATTbotss<><>' //change the watermark 
-global.image = 'https://telegra.ph/file/3378e6981054be96a8628.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/3378e6981054be96a8628.jpg'
+global.image = 'https://telegra.ph/file/aa2f830bfcfb6b8f0b91b.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/aa2f830bfcfb6b8f0b91b.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
-global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
-global.kolam = 'https://telegra.ph/file/3378e6981054be96a8628.jpg'
-global.thanks = 'https://telegra.ph/file/3378e6981054be96a8628.jpg'
+global.kandang = 'https://telegra.ph/file/aa2f830bfcfb6b8f0b91b.jpg'
+global.kolam = 'https://telegra.ph/file/aa2f830bfcfb6b8f0b91b.jpg'
+global.thanks = 'https://telegra.ph/file/aa2f830bfcfb6b8f0b91b.jpg'
 
 global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
 
